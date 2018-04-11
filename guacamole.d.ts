71925794155ea3fc37d2a125adbca6f8be532887
Jamie Hall <hall@ill.eu>
2018-04-11 11:44:06 +0200

    Added SockeTIO definition

--- guacamole.d.ts
+++ guacamole.d.ts
@@ -1,5 +1,193 @@
 declare module '@illgrenoble/guacamole-common-js' {
 
+
+    /**
+     * Dynamic on-screen keyboard. Given the layout object for an on-screen
+     * keyboard, this object will construct a clickable on-screen keyboard with its
+     * own key events.
+     */
+    class OnScreenKeyboard {
+        constructor(layout: OnScreenKeyboard.Layout);
+
+        /**
+         * The number of mousemove events to require before re-enabling mouse
+         * event handling after receiving a touch event.
+         *
+         * @type {Number}
+         */
+        touchMouseThreshold: number;
+
+        /**
+         * Fired whenever the user presses a key on this Guacamole.OnScreenKeyboard.
+         * 
+         * @event
+         * @param {Number} keysym The keysym of the key being pressed.
+         */
+        onkeydown(keysym: number): void;
+        /**
+         * Fired whenever the user releases a key on this Guacamole.OnScreenKeyboard.
+         * 
+         * @event
+         * @param {Number} keysym The keysym of the key being released.
+         */
+        onkeyup(keysym: number): void;
+
+        /**
+         * The keyboard layout provided at time of construction.
+         *
+         * @type {Guacamole.OnScreenKeyboard.Layout}
+         */
+        layout: OnScreenKeyboard.Layout;
+
+        /**
+         * Returns the element containing the entire on-screen keyboard.
+         * @returns {Element} The element containing the entire on-screen keyboard.
+         */
+        getElement(): any;
+
+        /**
+         * Resizes all elements within this Guacamole.OnScreenKeyboard such that
+         * the width is close to but does not exceed the specified width. The
+         * height of the keyboard is determined based on the width.
+         * 
+         * @param {Number} width The width to resize this Guacamole.OnScreenKeyboard
+         *                       to, in pixels.
+         */
+        resize(width: number): void;
+
+          /**
+         * Resets the state of this keyboard, releasing all keys, and firing keyup
+         * events for each released key.
+         */
+        reset(): void;
+    }
+
+
+    namespace OnScreenKeyboard {
+
+
+        class Layout {
+
+            /**
+             * @constructor
+             * @param {Guacamole.OnScreenKeyboard.Layout|Object} template
+             *     The object whose identically-named properties will be used to initialize
+             *     the properties of this layout.
+             */
+            constructor(template: any);
+            /**
+             * The language of keyboard layout, such as "en_US". This property is for
+             * informational purposes only, but it is recommend to conform to the
+             * [language code]_[country code] format.
+             *
+             * @type {String}
+             */
+            language: string;
+
+            /**
+             * The type of keyboard layout, such as "qwerty". This property is for
+             * informational purposes only, and does not conform to any standard.
+             *
+             * @type {String}
+             */
+            type: string;
+
+            /**
+             * Map of key name to corresponding keysym, title, or key object. If only
+             * the keysym or title is provided, the key object will be created
+             * implicitly. In all cases, the name property of the key object will be
+             * taken from the name given in the mapping.
+             *
+             * @type {Object.<String, Number|String|Guacamole.OnScreenKeyboard.Key|Guacamole.OnScreenKeyboard.Key[]>}
+             */
+            keys: any;
+
+            /**
+             * Arbitrarily nested, arbitrarily grouped key names. The contents of the
+             * layout will be traversed to produce an identically-nested grouping of
+             * keys in the DOM tree. All strings will be transformed into their
+             * corresponding sets of keys, while all objects and arrays will be
+             * transformed into named groups and anonymous groups respectively. Any
+             * numbers present will be transformed into gaps of that size, scaled
+             * according to the same units as each key.
+             *
+             * @type {Object}
+             */
+            layout: any;
+
+            /**
+             * The width of the entire keyboard, in arbitrary units. The width of each
+             * key is relative to this width, as both width values are assumed to be in
+             * the same units. The conversion factor between these units and pixels is
+             * derived later via a call to resize() on the Guacamole.OnScreenKeyboard.
+             *
+             * @type {Number}
+             */
+            width: number;
+
+            /**
+             * The width of each key, in arbitrary units, relative to other keys in
+             * this layout. The true pixel size of each key will be determined by the
+             * overall size of the keyboard. If not defined here, the width of each
+             * key will default to 1.
+             *
+             * @type {Object.<String, Number>}
+             */
+            keyWidths: any;
+        }
+
+        /**
+         * Represents a single key, or a single possible behavior of a key. Each key
+         * on the on-screen keyboard must have at least one associated
+         * Guacamole.OnScreenKeyboard.Key, whether that key is explicitly defined or
+         * implied, and may have multiple Guacamole.OnScreenKeyboard.Key if behavior
+         * depends on modifier states.
+         */
+        class Key {
+            constructor(template: any, name: string);
+
+            /**
+             * The unique name identifying this key within the keyboard layout.
+             *
+             * @type {String}
+             */
+            name: string;
+
+            /**
+             * The human-readable title that will be displayed to the user within the
+             * key. If not provided, this will be derived from the key name.
+             *
+             * @type {String}
+             */
+            title: string;
+
+            /** 
+             * The name of the modifier set when the key is pressed and cleared when
+             * this key is released, if any. The names of modifiers are distinct from
+             * the names of keys; both the "RightShift" and "LeftShift" keys may set
+             * the "shift" modifier, for example. By default, the key will affect no
+             * modifiers.
+             * 
+             * @type {String}
+             */
+            modifier: string;
+
+            /**
+             * An array containing the names of each modifier required for this key to
+             * have an effect. For example, a lowercase letter may require nothing,
+             * while an uppercase letter would require "shift", assuming the Shift key
+             * is named "shift" within the layout. By default, the key will require
+             * no modifiers.
+             *
+             * @type {String[]}
+             */
+            requires: string[];
+        }
+
+
+    }
+
+
     /**
      * Guacamole protocol client. Given a {@link Guacamole.Tunnel},
      * automatically handles incoming and outgoing Guacamole instructions via the
@@ -367,6 +555,12 @@ declare module '@illgrenoble/guacamole-common-js' {
      * @see Guacamole.HTTPTunnel
      */
     class Tunnel {
+        static INTERNAL_DATA_OPCODE: string;
+        static State: any;
+
+
+        uuid: string;
+
 
         /**
          * The current state of this tunnel.
@@ -455,6 +649,15 @@ declare module '@illgrenoble/guacamole-common-js' {
          * @param {String} tunnelURL The URL of the WebSocket tunneling service.
          */
         constructor(tunnelURL: string);
+    }
+
+
+    class SocketIOTunnel extends Tunnel {
+        constructor(url: string, connectionOptions: any, eventChannel: string);
+        /** 
+         * Return the socketio socket
+         */
+        getSocket(): any;
     }
 
     /**
